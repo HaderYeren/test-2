3463bcb032c929e1d820ded54e6624fa76c11663
HaderYeren <136634293+HaderYeren@users.noreply.github.com>
2024-10-30 18:00:57 +0200

    +

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-const getUserNames = users => users.map(({ name }) => name);
-console.log(getUserNames(users));
+const box = document.getElementById('box');
 
-const getUsersWithEyeColor = (users, color) => users.filter(({ eyeColor }) => eyeColor === color);
-console.log(getUsersWithEyeColor(users, 'blue'));
+const moveBox = (event) => {
+  box.style.left = `${event.pageX}px`;
+  box.style.top = `${event.pageY}px`;
+};
 
-const getUserNamesByGender = (users, gender) => users
-    .filter(user => user.gender === gender)
-    .map(({ name }) => name);
-console.log(getUserNamesByGender(users, 'female'));
-
-const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
-console.log(getInactiveUsers(users)); 
-
-const getUserByEmail = (users, email) => users.find(user => user.email === email);
-console.log(getUserByEmail(users, 'alice.johnson@example.com'));
-
-const getUsersByAgeRange = (users, min, max) => users.filter(({ age }) => age >= min && age <= max);
-console.log(getUsersByAgeRange(users, 26, 30));
-
-
-
-
-
-
+document.addEventListener('mousemove', debounce(moveBox, 100));
---
+++ slider.js
@@ -0,0 +1,16 @@
+const sliderInput = document.querySelector('.slider__input');
+const sliderImage = document.querySelector('.slider__image');
+
+const updateImageSize = (event) => {
+  const sliderValue = event.target.value;
+  sliderImage.style.width = `${sliderValue}%`;
+};
+
+sliderInput.addEventListener('input', debounce(updateImageSize, 150));
+
+
+
+
+
+
+
