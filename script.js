5a842a6babb9d06dd7d486b130b7ab6d02fa2841
HaderYeren <136634293+HaderYeren@users.noreply.github.com>
2024-12-04 18:44:31 +0200

    +

--- script.js
+++
@@ -1,8 +0,0 @@
-const box = document.getElementById('box');
-
-const moveBox = (event) => {
-  box.style.left = `${event.pageX}px`;
-  box.style.top = `${event.pageY}px`;
-};
-
-document.addEventListener('mousemove', _.debounce(moveBox, 100));
